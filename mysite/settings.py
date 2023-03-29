02ff13458ff9afc5eb9a37b8813eeec70f33e32e
tatsiana varanets <tatyana.voronec@mail.ru>
2023-03-29 15:17:28 +0300

    add media to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,11 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, "static"),
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.joinpath("static/")]
+STATIC_ROOT = BASE_DIR.joinpath("staticfiles/")
 STATICFILES_FINDERS = 'djangobower.finders.BowerFinder',
+
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

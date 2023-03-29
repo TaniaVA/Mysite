44da0e6844b4efadd0c270b40daaee06cf56324d
tatsiana varanets <tatyana.voronec@mail.ru>
2023-03-29 23:15:11 +0300

    add photo for users

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     pass
+
+class CustomerPhoto(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField()
+    photo = models.ImageField(upload_to='users/', default='users/profile-placeholder.png', blank=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
